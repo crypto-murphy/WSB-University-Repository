e57fe68e9452e075476a83349672a7865f3efcbc
crypto-murphy <mi8484@yahoo.com>
2022-11-08 16:34:51 +0100

    My first commit

---
+++ helloWorld.cpp
@@ -0,0 +1,17 @@
+/***************************************** 
+ Program name: helloWorld.cpp 
+ Author: Michal Mielewczyk, Drammen 2022
+ Created: 2022-11-08
+ Ver. 1.00 
+ *****************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "That's my 1st awesome program in C++! \n";
+
+    return 0;
+}
