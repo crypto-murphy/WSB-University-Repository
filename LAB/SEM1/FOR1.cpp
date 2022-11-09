acba01919599e4e1872f6c94aeaf0ac6a696c62d
crypto-murphy <mi8484@yahoo.com>
2022-11-09 22:15:37 +0100

    v1.00

---
+++ LAB/SEM1/FOR1.cpp
@@ -0,0 +1,28 @@
+/***************************************** 
+ Program name: FOR1.cpp 
+ Author: Michal Mielewczyk, Drammen 2022
+ Created: 2022-11-09
+ Ver. 1.00 
+ *****************************************/
+
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+
+using namespace std;
+
+string name; int x;
+
+int main()
+{
+    cout << "Type your name: ";
+    cin  >> name;
+    cout << "Type your number: ";
+    cin >> x;     
+      for(int i=1; i<=x; i++)
+{
+      Sleep(700);
+      cout << i << ". " << name << endl;
+}
+    return 0;
+}
