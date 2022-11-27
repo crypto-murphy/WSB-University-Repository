94116219279338acc89d2a42655b52c72e352bb9
crypto-murphy <mi8484@yahoo.com>
2022-11-27 16:25:56 +0100

    v1.00

--- LAB/SEM1/WdP-LAB2/zad4A_lab2.cpp
+++ LAB/SEM1/WdP-LAB2/zad4A_lab2.cpp
@@ -1,8 +1,9 @@
 /***************************************** 
- Program name: zad4_lab2.cpp 
+ Program name: zad4A_lab2.cpp 
  Author: Michal Mielewczyk, Drammen 2022
- Task 4: Napisz program wypisujący na ekranie w odrębnych liniach znak
+ Task 4: [ZADANIE 4] Napisz program wypisujący na ekranie w odrębnych liniach znak
  wraz z jego kodem ASCII, dla znaków ASCII o wartościach z zakresu od 32 do 128.
+   A. Zmodyfikuj program w ten sposób, aby w każdym wierszu była informacja na temat trzech kolejnych znaków.
  Created: 2022-11-20
  Ver. 1.00 
  *****************************************/
--- LAB/SEM1/WdP-LAB2/zad4B_lab2.cpp
+++ LAB/SEM1/WdP-LAB2/zad4B_lab2.cpp
@@ -1,8 +1,9 @@
 /***************************************** 
- Program name: zad4_lab2.cpp 
+ Program name: zad4B_lab2.cpp 
  Author: Michal Mielewczyk, Drammen 2022
  Task 4: Napisz program wypisujący na ekranie w odrębnych liniach znak
  wraz z jego kodem ASCII, dla znaków ASCII o wartościach z zakresu od 32 do 128.
+   B. Rozszerz program aby wypisywał jeszcze informacje dla znaków od 1 do 31.
  Created: 2022-11-20
  Ver. 1.00 
  *****************************************/
@@ -18,7 +19,13 @@ int main()
     for(int kod=32; kod<=128; kod++) 
     {
         znak = kod;
-        cout << kod << "\t" << kod << "\t" << znak << "\n";
+        cout << kod << "\t" << znak << "\n";
+    }
+
+    for(int kod=1; kod<=31; kod++) 
+    {
+        znak = kod;
+        cout << kod << "\t" << znak << "\n";
     }
 
     return 0;
