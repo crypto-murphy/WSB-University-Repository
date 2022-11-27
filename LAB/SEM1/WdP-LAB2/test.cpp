86d4fd8bc3d81f4d9460c657dc007419f04a3fd0
crypto-murphy <mi8484@yahoo.com>
2022-11-27 17:34:19 +0100

    v1.00

---
+++ LAB/SEM1/WdP-LAB2/test.cpp
@@ -0,0 +1,17 @@
+/***************************************** 
+ Program name: test.cpp 
+ Author: Michal Mielewczyk, Drammen 2022
+ Created: 2022-11-27
+ Ver. 1.00 
+ *****************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     //Michal, just relax and start coding here... :)
+
+    return 0;
+}
