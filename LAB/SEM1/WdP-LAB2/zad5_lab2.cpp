e83c9685d850fbde35f39441e84d206593cd5659
crypto-murphy <mi8484@yahoo.com>
2022-11-27 19:48:56 +0100

    v1.00

--- LAB/SEM1/WdP-LAB2/zad5_lab2.cpp
+++ LAB/SEM1/WdP-LAB2/zad5_lab2.cpp
@@ -3,6 +3,15 @@
  Author: Michal Mielewczyk, Drammen 2022
  Task 5: Napisz program, który pobierze od użytkownika 5 
  liczb rzeczywistych i umieści je w odpowiedniej tablicy:
+    A. Wypisz te wszystkie liczby w pojedynczej linii jedna po drugiej.
+    B. Zmodyfikuj program w ten sposób aby podczas wpisywania liczb liczył 
+       na bieżąco ich sumę i średnią i wypisywał na ekranie, np.: Wprowadź 
+       1 liczbę: 3.14 Suma wprowadzonych liczb: 3.14, średnia: 3.14 Wprowadź 
+       2 liczbę: 6.28 Suma wprowadzonych liczb: 9.42, średnia: 4.71
+    C. Zmodyfikuj kawałek kodu wypisujący wszystkie liczby w ten sposób, aby 
+       w przypadku liczb z zakresu od -5 do 5 (ale z wyłączeniem 0 ) program 
+       wypisywał tekst TAJNA a w pozostałych przypadkach wypisywał liczbę. 
+       Zapisz ten sam warunek przy pomocy innych operatorów.
  Created: 2022-11-20
  Ver. 1.00 
  *****************************************/
