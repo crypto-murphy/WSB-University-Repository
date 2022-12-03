7eb7c80ce4e7865707483e109e6aafff4f85f14b
crypto-murphy <mi8484@yahoo.com>
2022-12-03 17:29:36 +0100

    v1.00

---
+++ LAB/SEM1/WdI-LAB3/potegowanie.pas
@@ -0,0 +1,25 @@
+(***************************************** 
+ Program name: potegowanie.pas 
+ Author: Michal Mielewczyk, Drammen 2022
+ Created: 2022-12-03
+ Ver. 1.00 
+ *****************************************)
+
+program potegowanie.pas;
+
+var i, potega, x, y : Integer;
+   
+begin
+ write('Podaj liczbe: ');
+ readLn(y);
+
+ write('Podaj potege: ');
+ readln(potega);
+
+ x := 1;
+ for i:=1 to potega do
+   x := x * y;
+
+ WriteLn('Wynik potegowania: ', x);
+ 
+end.
