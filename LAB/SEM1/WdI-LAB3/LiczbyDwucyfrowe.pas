fd4f253bdce4798a74916b35f62ffbb230f3ebb0
crypto-murphy <mi8484@yahoo.com>
2022-12-03 18:17:22 +0100

    v1.00

---
+++ LAB/SEM1/WdI-LAB3/LiczbyDwucyfrowe.pas
@@ -0,0 +1,21 @@
+(***************************************** 
+ Program name: LiczbyDwucyfrowe.pas 
+ Author: Michal Mielewczyk, Drammen 2022
+ Created: 2022-12-03
+ Ver. 1.00 
+ *****************************************)
+
+program LiczbyDwucyfrowe;
+uses crt;
+var i, n : integer;
+begin
+  i := 10;
+  Write('Podaj liczbe: ');
+  Read(n);
+  while i < n do
+  begin
+    Writeln(i);
+    i := i + 1;
+  end;
+  readkey;
+end.
