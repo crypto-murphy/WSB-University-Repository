8c802cfa131721e14fc3491bdbc10d337282e91f
crypto-murphy <mi8484@yahoo.com>
2022-12-03 16:16:18 +0100

    v1.00

---
+++ LAB/SEM1/WdI-LAB3/miesiace.pas
@@ -0,0 +1,31 @@
+(***************************************** 
+ Program name: miesiace.pas 
+ Author: Michal Mielewczyk, Drammen 2022
+ Task 1: Napisz program, ktorey wczytuje numer miesiaca i wypisuje jego nazwe
+ Created: 2022-12-03
+ Ver. 1.00 
+ *****************************************)
+
+program miesiace;
+
+var m: integer;
+
+begin
+read(m);
+case m of
+    1: Write('styczen');
+    2: Write('luty');
+    3: Write('marzec');
+    4: Write('kwiecien');
+    5: Write('maj');
+    6: Write('czerwiec');
+    7: Write('lipiec');
+    8: Write('sierpien');
+    9: Write('wrzesien');
+    10: Write('pazdziernik');
+    11: Write('listopad');
+    12: Write('grudzien');
+else
+    write('Nie ma takiego miesiaca');
+    end;
+end.
