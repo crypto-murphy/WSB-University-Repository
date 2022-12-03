feb8b79651aaaa7c8b26efdc417b606689ccc949
crypto-murphy <mi8484@yahoo.com>
2022-12-03 19:08:31 +0100

    v1.00

---
+++ LAB/SEM1/WdI-LAB4/quiz2.pas
@@ -0,0 +1,23 @@
+(***************************************** 
+ Program name: quiz2.pas 
+ Author: Michal Mielewczyk, Drammen 2022
+ Task 9: Napisz program, ktorym zadasz pytanie i bedziesz wczytywal 
+         odpowiedz dopoki uzytkownik nie poda poprawnej odpowiedzi.
+ Created: 2022-12-03
+ Ver. 1.00 
+ *****************************************)
+
+program quiz2;
+uses crt;
+var odp : string;
+
+begin
+    repeat
+    begin
+    writeln('Stolica Polski jest: ');
+    readln(odp);
+    end;
+    until
+    lowercase(odp) = 'warszawa'; //  uppercase <-- WARSZAWA uodparniamy program przed wielkimi literami
+readkey;
+end.
